b291a5c754119e2623de05fd7a91ef2394280089
Eugene Korshenko <korshenko.ea@gmail.com>
2017-03-20 00:18:20 +0200

    - Build with webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const {IgnorePlugin, BannerPlugin} = require('webpack');
+const path = require('path');
+const fs = require('fs');
+
+let nodeModules = {};
+fs.readdirSync('node_modules')
+    .filter((x) => ['.bin'].indexOf(x) === -1)
+    .forEach(function (mod) {
+        nodeModules[mod] = 'commonjs ' + mod;
+    });
+
+module.exports = {
+    entry: './server/src/boot.ts',
+    target: 'node',
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'backend.js'
+    },
+    externals: nodeModules,
+    resolve: {
+        extensions: ['.ts']
+    },
+    module: {
+        loaders: [{
+            test: /\.ts?$/,
+            loader: "ts-loader"
+        }]
+    },
+    plugins: [
+        new IgnorePlugin(/\.(css|less)$/),
+        new BannerPlugin({banner: 'require("source-map-support").install();', raw: true, entryOnly: false})
+    ],
+    devtool: 'sourcemap'
+};
